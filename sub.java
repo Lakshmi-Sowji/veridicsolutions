1224e3a2e9ca28879071d37d6a247e75792b6bdb
lakshmi <lakshmi@gmail.com>
2019-02-26 14:17:34 -0600

    sub and subtraction are added on feb26 2019

---
+++ sub.java
@@ -0,0 +1,14 @@
+
+
+class Addition extends Subtraction
+{
+	int a = 10;
+	int b = 20;
+	int c = 30;
+	void sub()
+	{
+		System.out.println ( a-b-c );
+	}
+}
+	
+	
---
+++ subtraction.java
@@ -0,0 +1,28 @@
+
+
+class Addition extends Subtraction
+{
+	int a = 10;
+	int b = 20;
+	int c = 30;
+	void add()
+	{
+		System.out.println ( a+b+c );
+	}
+}
+	
+	
+
+
+class Addition extends Subtraction
+{
+	int a = 10;
+	int b = 20;
+	int c = 30;
+	void sub()
+	{
+		System.out.println ( a-b-c );
+	}
+}
+	
+	
